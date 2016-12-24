9e89f35a63b8cf17064d7c15e63ab2a6c42c0490
jzdynamo <jzdynamo@gmail.com>
2016-12-24 18:46:59 +0800

    Fix set_db.js.

--- src/main/set_db.js
+++ src/main/set_db.js
@@ -1,19 +1,20 @@
-const path = require('path'),
-  Datastore = require('nedb'),
-  db = {};
+import path from 'path';
+import Datastore from 'nedb';
 import constants from '../constants';
+
+const db = {};
 db.files = new Datastore({ filename: path.resolve(constants.DB_PATH, './files'),
   autoload: true,
-  timestampData: true }),
-  db.books = new Datastore({ filename: path.resolve(constants.DB_PATH, './books'),
-    autoload: true,
-    timestampData: true }),
-  db.tags = new Datastore({ filename: path.resolve(constants.DB_PATH, './tags'),
-    autoload: true,
-    timestampData: true }),
-  db.infos = new Datastore({ filename: path.resolve(constants.DB_PATH, './infos'),
-    autoload: true,
-    timestampData: true });
+  timestampData: true });
+db.books = new Datastore({ filename: path.resolve(constants.DB_PATH, './books'),
+  autoload: true,
+  timestampData: true });
+db.tags = new Datastore({ filename: path.resolve(constants.DB_PATH, './tags'),
+  autoload: true,
+  timestampData: true });
+db.infos = new Datastore({ filename: path.resolve(constants.DB_PATH, './infos'),
+  autoload: true,
+  timestampData: true });
 
 db.books.ensureIndex({ fieldName: 'updatedAt' });
 db.files.ensureIndex({ fieldName: 'updatedAt' });
