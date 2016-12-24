750ad4ead6ce9d251ef4f55ff02ab69d1325cf0d
jzdynamo <jzdynamo@gmail.com>
2016-12-24 17:03:39 +0800

    Fix one file's lint errors and add more rules.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,27 +1,23 @@
-Promise = require('bluebird');
-window.$ = window.jQuery = window.jquery = require('jquery');
-require('babel-polyfill');
-
-const fs = require('fs'),
-  path = require('path');
-
+/* global $ */
 import React from 'react';
 import { render } from 'react-dom';
-import App from './views/app';
-import { files, books, tags, infos } from '../main/set_db';
 import { Provider } from 'react-redux';
-import store from './store';
-import constants from '../constants';
-window.constants = constants;
-const { FILES_PATH } = constants;
-
 // material-ui use the plugin
 import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 // material-ui theme provider
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import App from './views/app';
+import store from './store';
+import constants from '../constants';
+
+window.$ = window.jQuery = window.jquery = require('jquery');
+require('babel-polyfill');
+
+window.constants = constants;
+const { FILES_PATH } = constants;
+
+injectTapEventPlugin();
 
 const muiTheme = getMuiTheme({
   fontFamily: '"YaHei Consolas Hybrid", Consolas, 微软雅黑, "Meiryo UI", "Malgun Gothic", "Segoe UI", "Trebuchet MS", Helvetica, Monaco, courier, monospace !important',
@@ -37,7 +33,7 @@ const ipcRenderer = require('electron').ipcRenderer;
 function controlNewNoteMenu() {
   let globalBook = null;
   store.subscribe(() => {
-    if ((!!store.getState().globalBook._id) != globalBook) {
+    if ((!!store.getState().globalBook._id) !== globalBook) {
       globalBook = (!!store.getState().globalBook._id);
       if (store.getState().globalBook._id) {
         ipcRenderer.send('enableItem', 'New Note');
@@ -64,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
 window.FILES_PATH = FILES_PATH;
 
 // prevent external link open from browser
-$('body').on('click', 'a.external', function (event) {
+$('body').on('click', 'a.external', (event) => {
   event.preventDefault();
   event.stopPropagation();
   const href = $(this).attr('href');
